814a514cad5d1a58094ce42d0b9c40fc4c8ffde6
StephaneWamba <stephane.wamba-tsakem-happi@efrei.net>
2026-08-10 22:03:39 +0200

    chore: aligne le contenu du parcours rendez-vous

---
+++ supabase/migrations/20260810000008_combined_booking_content.sql
@@ -0,0 +1,7 @@
+update site_content
+set value = 'Finalisez votre dossier et prenez rendez-vous'
+where key = 'rdv.header.title';
+
+update site_content
+set value = 'Après votre paiement, complétez votre profil puis choisissez directement votre créneau avec Charlène.'
+where key = 'rdv.header.description';
